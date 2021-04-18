730ee04c5c9720333487c978f161130df9797b48
Piotr Domian <domianpiotr111@gmail.com>
2021-04-18 20:22:23 +0200

    Task is done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sentenceToUpperCase() {
-  // Zaimplementuj zadanie zgodnie z wytycznymi
+function sentenceToUpperCase(str) 
+{
+  var a = str.split(" ");
+   
+  for(var i=0; i < a.length; i++)
+  {
+    a[i] = a[i].replace(a[i][0],a[i][0].toUpperCase());
+  }
+ return a.join(" ");
 };
 
 module.exports = sentenceToUpperCase;
+
